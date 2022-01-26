923298f7d16c61448e6b8a2c6b5b378b6bcef23c
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-01-26 15:39:09 +0900

    アイテム検索結果表示画面の作成

---
+++ wine_app/resources/views/wine-search.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.base')
+
+@section('header')
+    @include('parts.header.default')
+@endsection
+
+@section('side-bar')
+    @include('parts.side-bar.default')
+@endsection
+
+@section('section')
+    <!-- TODO ワイン検索条件でタイトルの変更 -->
+    <h2>赤ワイン</h2>
+    <div class="container-fluid col-lg-12 ">
+        <div class="row">
+            @for ($i = 0; $i < 12; $i++)
+                <div class="col-lg-3">
+                Column
+                </div>
+            @endfor
+        </div>
+        <!-- TODO Paginationの編集 -->
+        {{-- Pagination start --}}
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+                </li>
+            </ul>
+        </nav>
+        {{-- Pagination end --}}
+    </div>
+@endsection

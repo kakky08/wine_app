b0dd6bb64249411464ddd8825d22b21ccb2f83dd
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-01-26 15:32:44 +0900

    アイテム詳細画面の作成

--- wine_app/routes/web.php
+++ wine_app/routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('wine');
 });
+Route::get('/item', function () {
+    return view('item');
+});
 
 Auth::routes();
 

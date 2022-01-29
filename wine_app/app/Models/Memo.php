b54ff0c00ff561ae710dfa63d6f7a661432151d6
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-01-29 09:39:12 +0900

    Memoモデル、マイグレーションの作成

---
+++ wine_app/app/Models/Memo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Memo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+            'user_id',
+            'folder_id',
+            'country_id',
+            'type_id',
+            'grape_id',
+            'aroma_category_id',
+            'name',
+            'content',
+            'comprehensive_evaluation',
+            "flavor",
+            'bitter_taste',
+            'afterglow',
+            'taste',
+            'bodied',
+            'sweet_taste',
+            'fruit_taste',
+            'acidity_taste'
+    ];
+}
